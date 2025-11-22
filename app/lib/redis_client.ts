35abea4baaac628c90a1c80a6eeee68c445826b5
MUCHIRAJUNIOR <muchirajunior022@gmail.com>
2025-11-22 19:16:16 +0300

    Update functionality to fetch and set redis data

--- app/lib/redis_client.ts
+++ app/lib/redis_client.ts
@@ -1,6 +1,8 @@
+'use server';
+
 import { createClient } from 'redis';
 
-export const client = await createClient({
+const client = await createClient({
     username:  process.env.REDIS_USERNAME,
     password: process.env.REDIS_PASSWORD,
     socket: {
@@ -10,3 +12,14 @@ export const client = await createClient({
 }) .on("error", (err) => console.log("Redis Client Error", err))
   .connect();
 
+export async function setRedisData({key,value}:{key:string, value:any}) {
+    var response =  await client.set(key,value)
+    console.log(response);
+    return response;
+    
+}
+
+export async function getRedisData(params:string) {
+    var respose = await client.get(params)
+    return respose;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,56 @@
+'use client';
+
+import { useActionState, useEffect, useState } from "react";
+import { setRedisData, getRedisData } from "./lib/redis_client";
 
 export default function Home() {
+  const [state,submit,isPending] = useActionState(addFood,null);
+  const [foodName, setFoodName] =  useState('')
+  const [data,setData] = useState({
+    isLoading: false,
+    foodData: ''
+  })
+
+  async function getData() {
+    setData({...data, isLoading:true});
+    var response = await getRedisData('food')
+     setData({foodData: response ?? '', isLoading:false});
+  }
+
+  async function addFood() {
+    if(foodName.length <= 4)return;
+    var response = await setRedisData({key: 'food',value: foodName})
+    console.log(response);
+    getData()
+    return response;
+  }
+
+  useEffect(()=>{getData()},[])
+
+
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      Hello world
+    <div className="flex min-h-screen items-center justify-center flex-col">
+       <form className="flex max-w-5xl flex-wrap m-5 gap-5" action={submit}>
+          <input
+            type="text"
+            className="p-2 border border-gray-300 focus:outline-orange-800 rounded-lg w-100"
+            placeholder="Add Food"
+            value={foodName}
+            onChange={(v)=>setFoodName(v.target.value)}
+          />
+         { 
+          isPending ?
+          <span className="flex w-10 border border-4 border-orange-200 border-b-orange-800  rounded-full mx-auto  animate-spin"/> :
+          <button className="rounded-lg bg-orange-800 hover:bg-orange-900 text-white cursor-pointer px-5 " type="submit">Add Item</button>
+         }
+       </form>
+      { 
+        data.isLoading ? 
+        <span className="flex p-5 border border-4 border-orange-200 border-b-orange-800  rounded-full mx-auto  animate-spin"/> :
+        <section className="grid">
+          <div className="col p-4 m-2 shadow shadow-lg rounded-lg font-bold">{data.foodData.toUpperCase()}</div>
+        </section>
+        }
     </div>
   );
 }
