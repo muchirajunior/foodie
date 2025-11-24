ef16ddfd46488c663039ab26e90f4f801fc7184a
MUCHIRAJUNIOR <muchirajunior022@gmail.com>
2025-11-24 08:16:27 +0300

    client pre initialization

--- app/lib/redis_client.ts
+++ app/lib/redis_client.ts
@@ -1,14 +1,23 @@
 'use server';
 
-import { createClient } from 'redis';
+import { createClient, RedisClientType } from 'redis';
 
-export const  redisClient = async ()=> await createClient({
-    username:  process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-    socket: {
-        host: process.env.REDIS_HOST_URL,
-        port: Number.parseInt(process.env.REDIS_PORT ?? '0000')
-    }
-}) .on("error", (err) => console.log("Redis Client Error", err))
-  .connect();
+let client : RedisClientType | null = null; 
+
+export const  redisClient =  async () : Promise<RedisClientType> =>{
+    if(client != null) return client;
+    console.log('Connect to redis');
+    
+    client = await createClient({
+        username:  process.env.REDIS_USERNAME,
+        password: process.env.REDIS_PASSWORD,
+        socket: {
+            host: process.env.REDIS_HOST_URL,
+            port: Number.parseInt(process.env.REDIS_PORT ?? '0000')
+        }
+    }) .on("error", (err) => console.log("Redis Client Error", err))
+    .connect();
+
+    return client ?? createClient();
+}
 
